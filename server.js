7dadcaebd94e59a3885cdd9c2dae8fdb03d9bc03
Franco Jesus Mamani Pozo <mamanipozofrancojesus@gmail.com>
2014-07-22 16:17:19 -0400

    server.js created

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+	res.send("hello world");
+});
+var server = app.listen(3000, function () {
+	console.log("listening on port 	%d", server.address().port);
+});
